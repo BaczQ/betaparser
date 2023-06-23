a97d93a7658dc1ad1670e67865aca458ac72c7c4
Aleksey Scherbakov <baczqueek@list.ru>
2023-06-24 02:08:24 +0700

    1

--- checkAkorda.js
+++ checkAkorda.js
@@ -1,19 +1,16 @@
 import Parser from "rss-parser";
 import { promises as fs } from "fs";
 
-
-
 const parser = new Parser();
 
 let knownPosts = []; // список уже известных постов
 
 export async function checkAkorda(bot) {
+  const currentDate = new Date();
+  const hours = String(currentDate.getHours()).padStart(2, "0");
+  const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+  const seconds = String(currentDate.getSeconds()).padStart(2, "0");
 
-    const currentDate = new Date();
-    const hours = String(currentDate.getHours()).padStart(2, "0");
-    const minutes = String(currentDate.getMinutes()).padStart(2, "0");
-    const seconds = String(currentDate.getSeconds()).padStart(2, "0");
-  
   try {
     const feed = await parser.parseURL("https://www.akorda.kz/ru/rss");
 
@@ -24,10 +21,8 @@ export async function checkAkorda(bot) {
       (item) => !knownPosts.includes(item.link)
     );
     if (newPosts.length > 0) {
-        console.log(`[${hours}:${minutes}:${seconds}] На сайте Акорда нет новых постов.`);
-      bot.sendMessage(
-        "@test222d",
-        `${hours}:${minutes}:${seconds} <b>Найдено ${newPosts.length} новых постов на сайте Акорда:</b>`
+      console.log(
+        `[${hours + 7}:${minutes}:${seconds}] На сайте Акорда Новые посты`
       );
       newPosts.forEach((post) => {
         console.log(`${post.title}: ${post.link}`);
@@ -41,18 +36,21 @@ export async function checkAkorda(bot) {
       // Запись списка известных постов в файл known_posts.txt
       await fs.writeFile("known_posts.txt", knownPosts.join("\n"));
     } else {
-        const currentDate = new Date();
-        console.log(`[${hours}:${minutes}:${seconds}] На сайте Акорда нет новых постов.`);
+      const currentDate = new Date();
+      console.log(
+        `[${hours + 7}:${minutes}:${seconds}] На сайте Акорда нет новых постов.`
+      );
     }
   } catch (err) {
     console.error("Ошибка при получении RSS-ленты сайта Акорда: \n", err);
-    bot.sendMessage(
+    /*bot.sendMessage(
       "@test222d",
       "Ошибка при получении RSS-ленты сайта Акорда:" + err
-    );
+    );*/
+
     setTimeout(() => {
-      console.log("повторяем");
+      console.log("Повторяем");
       checkAkorda(bot);
-    }, 10000); // повторяем через 10 секунд
+    }, 300000); // повторяем через 5 мин
   }
 }
--- index.js
+++ index.js
@@ -1,13 +1,11 @@
+import TelegramBot from "node-telegram-bot-api";
+import { checkAkorda } from "./checkAkorda.js";
 
-import TelegramBot from 'node-telegram-bot-api';
-import {checkAkorda} from './checkAkorda.js';
-
-const token = '6250798032:AAGgelPs4SjqQCUsRlNOL9_zepLhc6ItK30';
+const token = "6250798032:AAGgelPs4SjqQCUsRlNOL9_zepLhc6ItK30";
 const bot = new TelegramBot(token, { polling: true });
- 
-async function runBot() {
 
-  console.log(' ');
+async function runBot() {
+  console.log(" ");
 
   await checkAkorda(bot);
 
@@ -15,3 +13,11 @@ async function runBot() {
 }
 
 runBot();
+setInterval(sendOkMessage, 14400000);
+
+// Функция для отправки сообщения "ок"
+function sendOkMessage() {
+  bot.sendMessage("@test222d", "Программа работает хорошо", {
+    disable_notification: true,
+  });
+}
